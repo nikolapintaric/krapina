d0343200437f9b588b55fb0d2d6d676ca0b26137
Nikola Pintaric <nikola.pintaric@gmail.com>
2015-01-08 23:42:23 +0100

    more particle work

--- src/THEGAME/particle/Emitter.java
+++ src/THEGAME/particle/Emitter.java
@@ -1,36 +1,49 @@
 package THEGAME.particle;
 
+import THEGAME.DrawableEntity;
+import org.lwjgl.util.vector.Vector2f;
+
 import java.util.ArrayList;
 
 /**
  * Created by nikola on 08.01.15..
  */
 public class Emitter {
-    private ArrayList<Particle> particles;
+    private ArrayList<DrawableEntity> particles;
     private ArrayList<Affector> affectors;
     private int firstFree=0;
+    public Vector2f pos;
+    public int maxParticles = 100;
+    public float milisecondSpeed;
+    public float lastParticle;
 
-
-    public Emitter(){
-        particles = new ArrayList<Particle>();
+    public Emitter(Vector2f pos, float milisecondSpeed){
+        particles = new ArrayList<DrawableEntity>();
         affectors = new ArrayList<Affector>();
-
+        this.pos = pos;
+        lastParticle = System.nanoTime() / 1000000.0f;
+        this.milisecondSpeed = milisecondSpeed;
     }
 
     public void addParticle(Particle particle){
-        if(particles.get(firstFree) == null)
-            particles.add(particle);
-        else
-            particles.set(firstFree, particle);
+        particles.set(firstFree, particle);
+        firstFree++;
+        if(firstFree == maxParticles - 1)
+            firstFree = 0;
+
     }
 
     public void addAffector(Affector affector){
         affectors.add(affector);
     }
 
-    public void update(){
+    public void update(float dt){
         for(Affector a:affectors){
-            a.update(particles);
+            a.update(particles, dt);
+        }
+        if(System.nanoTime() / 1000000.0f - lastParticle > milisecondSpeed){
+            lastParticle = System.nanoTime() / 1000000.0f;
+            addParticle(new Particle(pos));
         }
     }
 
