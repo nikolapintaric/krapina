fe8ed282af17676aceec2c4650fe643f22c5841d
Mihael Liskij <mihael.liskij@gmail.com>
2015-01-08 21:48:44 +0100

    Started removing unnecessary code.

--- src/THEGAME/Quad.java
+++ src/THEGAME/Quad.java
@@ -15,60 +15,17 @@ import org.newdawn.slick.util.ResourceLoader;
 
 public class Quad extends MovableEntity {
 
-
-
-
         public Vector2f size;
-        private Texture texture1, texture2, texture3;
-        public int tmp;
+        private String texture;
 
         private int vboId, vaoId, vertexCount;
 
         public Quad(){
-            //this(10, 10);
         }
 
         public Quad(float x, float y){
             size = new Vector2f(x, y);
             position = new Vector2f(0, 0);
-            float[] vertices = {
-                    0f, 0f, 0f,
-                    size.x, 0f, 0f,
-                    0f, size.y, 0f,
-                    0f, size.y, 0f,
-                    size.x, 0f, 0f,
-                    size.x, size.y, 0f
-            };
-
-            try {
-                texture1 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/background.png"));
-                texture2 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/background_2.png"));
-                texture3 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/background_3.png"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-            /*
-            FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
-            verticesBuffer.put(vertices);
-            verticesBuffer.flip();
-
-            vertexCount = 6;
-
-
-            vaoId = glGenVertexArrays();
-            glBindVertexArray(vaoId);
-
-            vboId = glGenBuffers();
-            glBindBuffer(GL_ARRAY_BUFFER, vboId);
-            glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            glBindVertexArray(0);
-            */
-
-
         }
 
         public void addForce(float x, float y){
@@ -76,37 +33,14 @@ public class Quad extends MovableEntity {
             velocity.y+=y;
         }
 
-        public void tick(){
-
-        }
-
         public void draw(){
             glPushMatrix();
             glTranslatef(position.x, position.y, 0);
 
-
-            /*glBindVertexArray(vaoId);
-            glEnableVertexAttribArray(0);
-
-            glDrawArrays(GL_TRIANGLES, 0, vertexCount);
-
-            glDisableVertexAttribArray(0);
-            glBindVertexArray(0);
-
-            glLoadIdentity();*/
-
-//           texture.bind();
             glEnable(GL_TEXTURE_2D);
-            if( tmp == 0 ){
-                glBindTexture(GL_TEXTURE_2D, texture1.getTextureID());
-            }
-            if( tmp == 1 ){
-                glBindTexture(GL_TEXTURE_2D, texture2.getTextureID());
-            }
-            if( tmp == 2 ){
-                glBindTexture(GL_TEXTURE_2D, texture3.getTextureID());
-            }
-//            glColor3f(1.0f, 1.0f, 1.0f);
+                //TODO: call use texture
+                //glBindTexture(GL_TEXTURE_2D, texture2.getTextureID());
+
             glBegin(GL_TRIANGLE_FAN);
                 glVertex2f(0.0f, 0.0f);
                 glTexCoord2f(0.0f,0.0f);
@@ -118,6 +52,7 @@ public class Quad extends MovableEntity {
                 glTexCoord2f(0.0f, 1.0f);
             glEnd();
 
+            glDisable(GL_TEXTURE_2D);
             glPopMatrix();
         }
 
