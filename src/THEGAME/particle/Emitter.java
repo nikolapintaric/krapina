803e9dbd0aeddd72d2ff93153cbd8fbd36f20c37
Nikola Pintaric <nikola.pintaric@gmail.com>
2015-01-08 22:51:22 +0100

    Added affectors

--- src/THEGAME/particle/Emitter.java
+++ src/THEGAME/particle/Emitter.java
@@ -6,12 +6,37 @@ import java.util.ArrayList;
  * Created by nikola on 08.01.15..
  */
 public class Emitter {
-    ArrayList<Particle> particles;
-    ArrayList<Affector> affectors;
+    private ArrayList<Particle> particles;
+    private ArrayList<Affector> affectors;
+    private int firstFree=0;
+
 
     public Emitter(){
-
+        particles = new ArrayList<Particle>();
+        affectors = new ArrayList<Affector>();
 
     }
+
+    public void addParticle(Particle particle){
+        if(particles.get(firstFree) == null)
+            particles.add(particle);
+        else
+            particles.set(firstFree, particle);
+    }
+
+    public void addAffector(Affector affector){
+        affectors.add(affector);
+    }
+
+    public void update(){
+        for(Affector a:affectors){
+            a.update(particles);
+        }
+    }
+
+
+
+
+
 
 }
