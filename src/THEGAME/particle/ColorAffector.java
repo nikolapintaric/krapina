3c98a0c9883ed8b649bda383a88c44b059e62726
Nikola Pintaric <nikola.pintaric@gmail.com>
2015-01-09 15:25:18 +0100

    Added Color back

--- src/THEGAME/particle/ColorAffector.java
+++ src/THEGAME/particle/ColorAffector.java
@@ -1,7 +1,9 @@
 package THEGAME.particle;
 
 import THEGAME.DrawableEntity;
+import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
+import org.lwjgl.util.vector.Vector4f;
 
 import java.util.ArrayList;
 
@@ -24,10 +26,9 @@ public class ColorAffector extends Affector{
     }
 
     public void update(ArrayList<DrawableEntity> particles, float dt){
-        for(DrawableEntity p:particles)
-            p.color.set((int) (p.color.x * colorMult.x * rand())
-                    , (int) (p.color.y * colorMult.y * rand())
-                    , (int) (p.color.z * colorMult.z * rand()), 1.0f);
+        for(DrawableEntity p:particles) {
+
+        }
     }
 
 }
